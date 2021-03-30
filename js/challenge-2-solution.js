e4928ac2e2fdf4fc7666b57a8aec2b0708e6f686
Bader-eng <baderali2016eng@gmail.com>
2021-03-30 13:30:44 +0300

    q2

---
+++ js/challenge-2-solution.js
@@ -0,0 +1,28 @@
+'use scrict'
+let q1=prompt('gess the number')
+let q2=prompt('gess the number')
+let q3=prompt('gess the number')
+let q4=prompt('gess the number')
+let q5=prompt('gess the number')
+
+if(q1>q2 & q1>q3 & q1>q4 & q1>q5   ){
+    alert('maxmum:'+q1)
+}else if(q1<q2 & q1<q3 & q1<q4 & q1<q5){
+    alert('min:' +q1)
+}else if(q2>q1 & q2>q3 & q2>q4 & q2>q5){
+    alert('maxmum:'+q2)
+}else if(q2<q1 & q2<q3 & q2<q4 & q2<q5){
+    alert('min:' +q2)
+}else if(q3>q1 & q3>q2 & q3>q4 & q3>q5){
+    alert('maxmum:'+q3)
+}else if(q3<q1 & q3<q2 & q3<q4 & q3<q5){
+    alert('min:' +q3)
+}else if(q4>q1 & q4>q3 & q4>q2 & q4>q5){
+    alert('maxmum:'+q4)
+}else if(q4<q1 & q4<q3 & q4<q2 & q4<q5){
+    alert('min:' +q4)
+}else if(q5>q1 & q5>q3 & q5>q4 & q5>q2){
+    alert('maxmum:'+q5)
+}else if(q5<q1 & q5<q3 & q5<q4 & q5<q2){
+    alert('min:' +q5)}
+    
